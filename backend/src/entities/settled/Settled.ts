740b0d972aa2970cf14382d749b1cf30b020596a
ChaitanyaRankhamb54 <chaitanyarankhamb007@gmail.com>
2026-02-06 11:56:51 +0530

    defined settled entities as class and repositories.

---
+++ backend/src/entities/settled/Settled.ts
@@ -0,0 +1,16 @@
+import { GroupId } from "../group/GroupId";
+import { UserId } from "../user/UserId";
+import { SettledId } from "./SettledId";
+
+export class Settled {
+  constructor(
+    public readonly _id: SettledId,
+    public readonly groupId: GroupId,
+    public who: UserId,
+    public whom: UserId,
+    public amount: number,
+    public isSettled: boolean = false,
+    public readonly creditedAt: Date,
+    public readonly updatedAt: Date
+  ) {}
+}
